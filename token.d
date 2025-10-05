8879dac1f20e1cf6f12737df65115fce1d21866a
kamstand <kh84590@gmail.com>
2025-10-05 17:26:40 -0400

    test(token): create new test to verify lexeme and byte slice

--- token.d
+++ token.d
@@ -45,16 +45,71 @@ unittest
 
         if (isDigit(input[i]))
         {
-            tokens[index++] = Token(TokenType.Int, i, i);
+            tokens[index++] = Token(TokenType.Int, i, i, input[i .. i + 1]);
         }
         else
         {
-            tokens[index++] = Token(TokenType.Plus, i, i);
+            tokens[index++] = Token(TokenType.Plus, i, i, input[i .. i + 1]);
         }
 
     }
     tokens[index] = Token(TokenType.Eof, i, i);
     assert(tokens[3].type == TokenType.Eof);
     assert(tokens[0].type == TokenType.Int);
+
+}
+
+unittest
+{
+    byte[] input = ['1', '+', '2'];
+    enum MAX_SIZE = 4;
+    Token[MAX_SIZE] tokens;
+    size_t index = 0;
+    size_t i = 0;
+    for (i = 0; i < 3; i++)
+    {
+        import std.ascii;
+
+        if (isDigit(input[i]))
+        {
+            tokens[index++] = Token(TokenType.Int, i, i, input[i .. i + 1]);
+        }
+        else
+        {
+            tokens[index++] = Token(TokenType.Plus, i, i, input[i .. i + 1]);
+        }
+
+    }
+    tokens[index] = Token(TokenType.Eof, i, i);
+    assert(tokens[0].slice == input[0 .. 1]);
+    assert(tokens[2].slice == input[2 .. $]);
+
+}
+
+unittest
+{
+    byte[] input = ['3', '+', '4'];
+    enum MAX_SIZE = 4;
+    Token[MAX_SIZE] tokens;
+    size_t index = 0;
+    size_t i = 0;
+    for (i = 0; i < 3; i++)
+    {
+        import std.ascii;
+
+        if (isDigit(input[i]))
+        {
+            tokens[index++] = Token(TokenType.Int, i, i, input[i .. i + 1]);
+        }
+        else
+        {
+            tokens[index++] = Token(TokenType.Plus, i, i, input[i .. i + 1]);
+        }
+
+    }
+    tokens[index] = Token(TokenType.Eof, i, i);
+    assert(cast(string) tokens[0].slice == "3");
+    assert(cast(string) tokens[1].slice != "-");
+    assert(cast(string) tokens[2].slice == "4");
 
 }
