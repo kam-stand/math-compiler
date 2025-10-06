14854c21bd2149c58915d0ec11b54711533d4ef4
kamstand <kh84590@gmail.com>
2025-10-05 21:22:17 -0400

    feat(lexer): lex all supported tokens

---
+++ lexer.d
@@ -0,0 +1,110 @@
+module lexer;
+import token;
+import std.ascii;
+import std.stdio;
+
+class Lexer
+{
+
+    Token*[] tokens;
+    byte[] input;
+    size_t index;
+    this(byte[] input)
+    {
+        this.input = input;
+        this.index = 0;
+        this.tokenize();
+    }
+
+    void tokenize()
+    {
+        while (index < input.length)
+        {
+            auto c = input[index];
+
+            switch (c)
+            {
+            case '+':
+                tokens ~= new Token(TokenType.Plus, input[index .. index + 1]);
+                index++;
+                break;
+
+            case '-':
+                tokens ~= new Token(TokenType.Minus, input[index .. index + 1]);
+                index++;
+                break;
+
+            case '*':
+                tokens ~= new Token(TokenType.Asterisk, input[index .. index + 1]);
+                index++;
+                break;
+
+            case '/':
+                tokens ~= new Token(TokenType.Slash, input[index .. index + 1]);
+                index++;
+                break;
+
+            default:
+                if (isDigit(c))
+                {
+                    size_t start = index;
+
+                    while (index < input.length && isDigit(input[index]))
+                        index++;
+
+                    auto numText = input[start .. index];
+                    tokens ~= new Token(TokenType.Int, numText);
+                }
+                else
+                {
+                    tokens ~= new Token(TokenType.Illegal, input[index .. index + 1]);
+                    index++;
+                }
+                break;
+            }
+        }
+
+        tokens ~= new Token(TokenType.Eof, null);
+    }
+
+}
+
+unittest
+{
+    byte[] input = ['*', '+', '/'];
+
+    Lexer l = new Lexer(input);
+
+    auto t = l.tokens;
+
+    assert(t.length != 0);
+    assert(t[3].type == TokenType.Eof);
+
+}
+
+unittest
+{
+    byte[] input = ['1', '+', '2', '3'];
+
+    Lexer l = new Lexer(input);
+
+    auto t = l.tokens;
+    assert(t.length == 4);
+    assert(t[0].type == TokenType.Int);
+    assert(t[3].type == TokenType.Eof);
+    assert(cast(string) t[2].slice == "23");
+
+}
+
+unittest
+{
+    byte[] input = ['1', '#', '2', '3'];
+
+    Lexer l = new Lexer(input);
+
+    auto t = l.tokens;
+    assert(t.length == 4);
+    assert(t[0].type == TokenType.Int);
+    assert(t[1].type == TokenType.Illegal);
+
+}
