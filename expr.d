ebb68e03871dba82b9b58d899d99131382d2d261
kamstand <kh84590@gmail.com>
2025-10-06 10:23:05 -0400

    feat(expr): add infix and prefix expression for ast

---
+++ expr.d
@@ -0,0 +1,94 @@
+module expr;
+import token;
+
+enum ExpressionType
+{
+    Literal,
+    Prefix,
+    Infix,
+}
+
+struct Literal
+{
+    Token* num;
+}
+
+struct Prefix
+{
+    Token* op;
+    Expression* rhs;
+}
+
+struct Infix
+{
+    Expression* lhs;
+    Token* op;
+    Expression* rhs;
+}
+
+struct Expression
+{
+    ExpressionType type;
+    union
+    {
+        Literal* lit;
+        Prefix* prefix;
+        Infix* infix;
+    }
+}
+
+Expression* makeLiteral(Token* num)
+{
+    auto literal = new Literal(num);
+    auto expr = new Expression();
+    expr.type = ExpressionType.Literal;
+    expr.lit = literal;
+    return expr;
+}
+
+Expression* makePrefix(Token* op, Expression* rhs)
+{
+    auto prefix = new Prefix(op, rhs);
+    auto expr = new Expression();
+    expr.type = ExpressionType.Prefix;
+    expr.prefix = prefix;
+    return expr;
+
+}
+
+Expression* makeInfix(Expression* lhs, Token* op, Expression* rhs)
+{
+    auto infix = new Infix(lhs, op, rhs);
+    auto expr = new Expression();
+    expr.type = ExpressionType.Infix;
+    expr.infix = infix;
+    return expr;
+
+}
+
+unittest
+{
+    byte[] input = ['1'];
+    auto lit = makeLiteral(new Token(TokenType.Int, input));
+    assert(lit.type is ExpressionType.Literal);
+}
+
+unittest
+{
+    byte[] input = ['-', '2'];
+    auto pre = makePrefix(new Token(TokenType.Minus, input[0 .. 1]), makeLiteral(
+            new Token(TokenType.Int, input[1 .. $])));
+    assert(pre.type is ExpressionType.Prefix);
+    assert(pre.prefix.op.type is TokenType.Minus);
+}
+
+unittest
+{
+    byte[] input = ['1', '+', '2'];
+    auto lhs = makeLiteral(new Token(TokenType.Int, input[0 .. 1]));
+    auto op = (new Token(TokenType.Plus, input[1 .. 2]));
+    auto rhs = makeLiteral(new Token(TokenType.Int, input[2 .. $]));
+    auto infix = makeInfix(lhs, op, rhs);
+    assert(infix.type is ExpressionType.Infix);
+    assert(infix.infix.rhs.type is ExpressionType.Literal);
+}
